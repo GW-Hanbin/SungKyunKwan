f935d5902ee081b2e0f9cd9a4065ddf7e880f8a2
GW-Hanbin <80751397+GW-Hanbin@users.noreply.github.com>
2021-06-19 01:56:41 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -6,93 +6,93 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def beer_crawl(driver, beer, data, k):
- data = pd.DataFrame(data=[], columns=['맥주정보', '검색이름', '맥주이름'])
+    data = pd.DataFrame(data=[], columns=['맥주정보', '검색이름', '맥주이름'])
 
- print('url_open... {0} 맥주 데이터 수집'.format(beer))
- driver = webdriver.Chrome(chromedriver)
- driver.get(url)
- driver.set_window_size(900, 900)
+    print('url_open... {0} 맥주 데이터 수집'.format(beer))
+    driver = webdriver.Chrome(chromedriver)
+    driver.get(url)
+    driver.set_window_size(900, 900)
 
- time.sleep(2)
- element = driver.find_element_by_xpath('//*[@id="root"]/div[2]/header/div[2]/div[1]/div[2]/div/div/input')
- time.sleep(2)
- element.click()
- time.sleep(2)
- element.send_keys(beer)
- time.sleep(2)
+    time.sleep(2)
+    element = driver.find_element_by_xpath('//*[@id="root"]/div[2]/header/div[2]/div[1]/div[2]/div/div/input')
+    time.sleep(2)
+    element.click()
+    time.sleep(2)
+    element.send_keys(beer)
+    time.sleep(2)
 
- driver.find_element_by_xpath('//*[@id="root"]/div[2]/header/div[2]/div[1]/div[2]/div/div[2]/a[1]/div/div[2]').click()
- time.sleep(2)
- beer_name = driver.find_element_by_css_selector('.MuiTypography-root.Text___StyledTypographyTypeless-bukSfn.pzIrn.text-500.colorized__WrappedComponent-hrwcZr.hwjOn.mt-3.MuiTypography-h4').text
- error_cnt = 0
+    driver.find_element_by_xpath('//*[@id="root"]/div[2]/header/div[2]/div[1]/div[2]/div/div[2]/a[1]/div/div[2]').click()
+    time.sleep(2)
+    beer_name = driver.find_element_by_css_selector('.MuiTypography-root.Text___StyledTypographyTypeless-bukSfn.pzIrn.text-500.colorized__WrappedComponent-hrwcZr.hwjOn.mt-3.MuiTypography-h4').text
+    error_cnt = 0
 
- while 1:
-  try:
-   time.sleep(2)
-   string = driver.find_element_by_class_name('MuiTypography-root.Text___StyledTypographyTypeless-bukSfn.pzIrn.text-500.colorized__WrappedComponent-hrwcZr.hwjOn.MuiTypography-h6').text
+    while 1:
+     try:
+      time.sleep(2)
+      string = driver.find_element_by_class_name('MuiTypography-root.Text___StyledTypographyTypeless-bukSfn.pzIrn.text-500.colorized__WrappedComponent-hrwcZr.hwjOn.MuiTypography-h6').text
 
-   extract = re.compile('[0-9]*,*[0-9]+')
-   str_num = extract.findall(string)
-   str_num = str_num[0]
+      extract = re.compile('[0-9]*,*[0-9]+')
+      str_num = extract.findall(string)
+      str_num = str_num[0]
 
-   print('성공.')
-   break
-  except:
-   print('오류 발생. 재시작.')
+      print('성공.')
+      break
+     except:
+      print('오류 발생. 재시작.')
 
-   error_cnt += 1
+      error_cnt += 1
 
-   if error_cnt == 5:
-    print('연속된 오류. 다음 맥주로 넘어감.')
-    return
+      if error_cnt == 5:
+       print('연속된 오류. 다음 맥주로 넘어감.')
+       return
 
- if ',' in str_num:
-  str_num = str_num.split(',')
-  str_num = int(str_num[0] + str_num[1])
-  num = str_num
- else:
-  num = int(str_num)
+    if ',' in str_num:
+     str_num = str_num.split(',')
+     str_num = int(str_num[0] + str_num[1])
+     num = str_num
+    else:
+     num = int(str_num)
 
- time.sleep(2)
- element = driver.find_element_by_xpath('//*[@id="root"]/div[2]/div[2]/div/div/div/div[2]/div[4]/div/div[2]/div[1]/div[2]')
- time.sleep(2)
- driver.execute_script("arguments[0].click();", element)
- page_num = num // 15 + 1
+    time.sleep(2)
+    element = driver.find_element_by_xpath('//*[@id="root"]/div[2]/div[2]/div/div/div/div[2]/div[4]/div/div[2]/div[1]/div[2]')
+    time.sleep(2)
+    driver.execute_script("arguments[0].click();", element)
+    page_num = num // 15 + 1
 
- for i in range(page_num):
-  print(i + 1, '번째 페이지.')
+    for i in range(page_num):
+     print(i + 1, '번째 페이지.')
 
-  time.sleep(2)
-  beer_info = driver.find_elements_by_css_selector('.px-4.fj-s.f-wrap')
+     time.sleep(2)
+     beer_info = driver.find_elements_by_css_selector('.px-4.fj-s.f-wrap')
 
-  tmp = []
+     tmp = []
 
-  for i in range(len(beer_info)):
-   tmp.append(beer_info[i].text)
+     for i in range(len(beer_info)):
+      tmp.append(beer_info[i].text)
 
-  tmp = pd.DataFrame(data=tmp, columns=['맥주정보'])
-  tmp['맥주이름'] = beer_name
-  tmp['검색이름'] = beer
-  data = pd.concat([data, tmp])
+     tmp = pd.DataFrame(data=tmp, columns=['맥주정보'])
+     tmp['맥주이름'] = beer_name
+     tmp['검색이름'] = beer
+     data = pd.concat([data, tmp])
 
-  try:
-   element = driver.find_element_by_xpath('//button[@title="Next page"]/span[@class="MuiIconButton-label"]')
-   time.sleep(3)
-   driver.execute_script("arguments[0].click();", element)
-  except:
-   print('마지막 페이지.')
+     try:
+      element = driver.find_element_by_xpath('//button[@title="Next page"]/span[@class="MuiIconButton-label"]')
+      time.sleep(3)
+      driver.execute_script("arguments[0].click();", element)
+     except:
+      print('마지막 페이지.')
 
- if num != len(data):
-  data = data[:num]
+    if num != len(data):
+     data = data[:num]
 
- print('리뷰수 : ', num, '수집된 리뷰수 : ', len(data))
+    print('리뷰수 : ', num, '수집된 리뷰수 : ', len(data))
 
- result = pd.merge(data, beer_list, on='검색이름', how='left')
- result.to_csv("beer_n_" + str(k) + ".csv", encoding='utf-8-sig')
+    result = pd.merge(data, beer_list, on='검색이름', how='left')
+    result.to_csv("beer_n_" + str(k) + ".csv", encoding='utf-8-sig')
 
- driver.quit()
+    driver.quit()
 
- return result
+    return result
 
 beer_list = ['Goose Island Goose IPA', 'Goose Island 312 Urban Wheat Ale', 'Edelweiss Weissbier Hefetrüb',
              'Somersby Apple Cider', 'Apple Fox', 'Desperados', 'Egger Grapefruit Radler', 'Egger Radler',
